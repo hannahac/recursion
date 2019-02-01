2eb5616de8bf2d452cdff4c6dfc8ccc7313afb8e
hannahac <hannahapio@gmail.com>
2019-02-01 18:22:04 +0300

    add changes to file

--- recursions.py
+++ recursions.py
@@ -4,9 +4,9 @@ def power(number, power_to_raise):
     
     if power_to_raise > 1:
         power_to_raise = power_to_raise - 1
-        return number * number
-    else:
         return number * power(number, power_to_raise)
+    else:
+        return 1
 
 result = power(3, 8)
 print(result)        
